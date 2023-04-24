9b8d03eb480ecf6a1792ab378b20342da69e3fe5
viarotel <viarotel@qq.com>
2023-04-24 11:25:41 +0800

    update

--- src/utils/gtag/index.js
+++ src/utils/gtag/index.js
@@ -1,7 +1,13 @@
 // Google Analytics 网站数据流分析工具
-import { install, gtag } from 'ga-gtag'
 import { GoogleAnalyticsId } from '@/configs/index'
 
-install(GoogleAnalyticsId)
-gtag('js', new Date())
-gtag('config', GoogleAnalyticsId)
+if (!import.meta.env.SSR) {
+  (async () => {
+    const { install, gtag } = await import('ga-gtag').catch((e) =>
+      console.error(e))
+
+    install(GoogleAnalyticsId)
+    gtag('js', new Date())
+    gtag('config', GoogleAnalyticsId)
+  })()
+}
