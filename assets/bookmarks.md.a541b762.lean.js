0d059c4e0f10aa2aabf86b619a9a534448452b3d
viarotel <38282758+viarotel@users.noreply.github.com>
2023-04-26 11:30:21 +0000

    Deploying to docs from @ viarotel/viarotel.github.io@e5fb7d9c5643a932d277916477a5d00e3fb1fdd4 🚀

---
+++ assets/bookmarks.md.a541b762.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as a,R as t}from"./chunks/framework.23834729.js";const k=JSON.parse('{"title":"Bookmarks","description":"","frontmatter":{},"headers":[],"relativePath":"bookmarks.md","lastUpdated":1682508532000}'),p={name:"bookmarks.md"},o=t("",508),n=[o];function l(h,s,f,i,c,g){return r(),a("div",null,n)}const m=e(p,[["render",l]]);export{k as __pageData,m as default};
