5ac7d795134610d904abdcac2e31e2235c24ca34
viarotel <viarotel@qq.com>
2023-05-10 14:58:01 +0800

    update

--- src/notes/public/scripts/zsh/index.sh
+++ src/notes/public/scripts/zsh/index.sh
@@ -1,28 +1,60 @@
-declare -A remote_scripts=(
-  [1]="https://example.com/script1.sh"
-  [2]="https://example.com/script2.sh"
+#!/bin/bash
+
+echo "Scripts by viarotel v0.0.1"
+
+# 定义脚本对象
+declare -A scripts=(
+  ["Homebrew Installer"]="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh|bash"
+  ["Script 2"]="https://example.com/script2.sh"
 )
 
-# 解析命令行选项
-while getopts ":s:" opt; do
-  case $opt in
-    s)
-      script_option=$OPTARG
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
+default_script_option="Homebrew Installer"
+
+# 显示用户选择界面
+echo "请选择要加载的远程脚本选项："
+echo
+
+mapScripts=()
+index=1
+for key in "${(k)scripts[@]}"; do
+  echo "$index. $key"
+  mapScripts[$index]=$key
+  ((index++))
 done
 
-# 根据选项设置远程脚本
-if [[ -n $script_option && -n "${remote_scripts[$script_option]}" ]]; then
-  remote_script="${remote_scripts[$script_option]}"
-else
-  remote_script="${remote_scripts[$default_script_option]}"
+echo
+echo "q. 退出"
+
+# 读取用户选择
+echo -n "请输入选项： "
+read script_option
+
+# 检查退出选项
+if [[ $script_option == "q" ]]; then
+  echo "退出脚本成功"
+  exit 0
 fi
 
+# 根据选项设置远程脚本、脚本名称和解释器
+if [[ -n $script_option && -n "${mapScripts[$script_option]}" ]]; then
+  script_name="${mapScripts[$script_option]}"
+  script_URL="${scripts[$script_name]}"
+
+  remote_script="${script_URL%|*}"
+  if [[ "$remote_script" == "$script_URL" ]]; then
+    interpreter="bash"
+  else
+    interpreter="${script_URL##*|}"
+  fi
+else
+  echo "错误的选项, 已自动退出"
+  exit 0
+fi
+
+echo $script_name
+echo $remote_script
+echo $interpreter
+
 # 下载并执行远程脚本
-echo "Loading remote script: $remote_script"
-curl -sSf "$remote_script" | zsh
+echo "Loading remote script: $script_name"
+# sudo curl -fsS --progress-bar "$remote_script" | $interpreter
