2f18e592d66f478e28d36b5989a0ad636545a704
viarotel <viarotel@qq.com>
2023-10-18 10:13:19 +0800

    docs: 📝 更新项目信息

--- src/data/repos.data.js
+++ src/data/repos.data.js
@@ -2,11 +2,33 @@ import { ofetch } from 'ofetch'
 
 const baseURL = 'https://ungh.cc'
 
+const includes = [
+  'escrcpy',
+  'vite-uniapp-template',
+  'packages',
+  'environments',
+  'waline-service',
+  'vue-cli-uniapp',
+  'vue-apicloud-cli',
+  'bing-chat-live-open',
+  'blog-demo',
+]
+
 async function getRepos() {
-  const res = await ofetch(`${baseURL}/orgs/viarotel-org/repos`, {
-    method: 'GET',
-  }).catch(e => console.warn(e))
-  return res?.repos || []
+  const promises = includes.map(name =>
+    ofetch(`${baseURL}/repos/viarotel-org/${name}`, {
+      method: 'GET',
+    }).catch(e => console.warn(e)),
+  )
+
+  const res = await Promise.allSettled(promises)
+
+  const repos = res
+    .filter(item => item.status === 'fulfilled')
+    .map(item => item.value.repo)
+  // console.log('repos', repos)
+
+  return repos
 }
 
 export default {
